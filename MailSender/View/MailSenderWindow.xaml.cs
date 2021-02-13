adbc06b6b284ea18039f087e82bbad054558b9e5
Георгий <georgij.tar@yandex.ru>
2021-02-13 10:13:47 +0300

    Построение модели

--- MailSender/View/MailSenderWindow.xaml.cs
+++ MailSender/View/MailSenderWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MailSender
+namespace MailSender.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MailSenderWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MailSenderWindow : Window
     {
-        public MainWindow()
+        public MailSenderWindow()
         {
             InitializeComponent();
         }
